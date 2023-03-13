957113197df70387743279e92ea48ce94677f386
chia <chiaravisca@outlook.com>
2023-03-13 19:32:34 +0100

    First part completed.

--- sql_scripts/CreateStoredProcedures.sql
+++ sql_scripts/CreateStoredProcedures.sql
@@ -1,22 +1,137 @@
--- EXAMPLE PROCEDURE
-CREATE PROCEDURE USP_Example
-(
-    @Dnumber INT,
-    @EmpCount INT OUTPUT
-)
-    AS
+USE [master]
+GO
+
+USE [Company]
+GO
+
+-- Procedure A:
+CREATE PROCEDURE [dbo].usp_CreateDepartment (
+    @DName VARCHAR(50),
+    @MgrSSN NUMERIC(9)
+) AS
 BEGIN
-SELECT @EmpCount = COUNT(*)
-FROM Employee
-WHERE Dno= @Dnumber;
+    DECLARE @DNumber INT
+    DECLARE @MgrStartDate DATETIME
+
+    SET @DNumber = (SELECT ISNULL(MAX(DNumber), 0) + 1 FROM Department)
+
+    IF EXISTS(SELECT * FROM Department WHERE DName = @DName)
+        BEGIN
+            THROW 51000, 'Department name already exists.', 1
+        END
+
+    IF EXISTS(SELECT * FROM Department WHERE MgrSSN = @MgrSSN)
+        BEGIN
+            THROW 51000, 'The manager is already a department manager.', 1
+        END
+
+    SET @MgrStartDate = GETDATE()
+
+    INSERT INTO Department (DName, DNumber, MgrSSN, MgrStartDate)
+    VALUES (@DName, @DNumber, @MgrSSN, @MgrStartDate)
+
+    SELECT @DNumber AS 'DNumber'
 END
+GO
 
--- PROCEDURES TODO: following
--- CREATE PROCEDURE usp_CreateDepartment(DName, MgrSSN)
--- CREATE PROCEDURE usp_UpdateDepartmentName(DNumber, DName)
--- CREATE PROCEDURE usp_UpdateDepartmentManager(DNumber, MgrSSN)
--- CREATE PROCEDURE usp_DeleteDepartment(DNumber)
--- CREATE PROCEDURE usp_GetDepartment(DNumber)
--- CREATE PROCEDURE usp_GetAllDepartments
+-- Procedure B:
+CREATE PROCEDURE [dbo].usp_UpdateDepartmentName (
+    @DNumber INT,
+    @DName VARCHAR(50)
+) AS
+BEGIN
+    IF EXISTS(SELECT * FROM Department WHERE DNumber = @DNumber)
+        BEGIN
+            IF EXISTS(SELECT * FROM Department WHERE DName = @DName)
+                BEGIN
+                    THROW 51000, 'Department name already exists.', 1
+                END
+            ELSE
+                BEGIN
+                    UPDATE Department SET DName = @DName WHERE DNumber = @DNumber
+                    SELECT 'Department name updated successfully! :)' AS 'Message'
+                END
+        END
+    ELSE
+        BEGIN
+            THROW 51000, 'Department number not found.', 1
+        END
+END
+GO
 
+-- Procedure C:
+CREATE PROCEDURE [dbo].usp_UpdateDepartmentManager (
+    @DNumber INT,
+    @MgrSSN NUMERIC(9)
+) AS
+BEGIN
+    IF EXISTS(SELECT * FROM Department WHERE DNumber = @DNumber)
+        BEGIN
+            IF EXISTS(SELECT * FROM Employee WHERE SSN = @MgrSSN)
+                BEGIN
+                    IF NOT EXISTS(SELECT * FROM Department WHERE MgrSSN = @MgrSSN)
+                        BEGIN
+                            UPDATE Department SET MgrSSN = @MgrSSN, MgrStartDate = GETDATE() WHERE DNumber = @DNumber
+                            UPDATE Employee SET SuperSSN = @MgrSSN WHERE Dno = @DNumber AND SSN NOT IN (SELECT MgrSSN FROM Department)
+                            SELECT 'Department Manager updated successfully! :)' AS 'Message'
+                        END
+                    ELSE
+                        BEGIN
+                            THROW 51000, 'Manager already manages another department.', 1
+                        END
+                END
+            ELSE
+                BEGIN
+                    THROW 51000, 'Manager SSN does not exist in the Employee table.', 1
+                END
+        END
+    ELSE
+        BEGIN
+            THROW 51000, 'Department number not found.', 1
+        END
+END
+GO
+
+-- Procedure D:
+CREATE PROCEDURE [dbo].usp_DeleteDepartment (@DNumber INT) AS
+BEGIN
+    IF EXISTS(SELECT * FROM Department WHERE DNumber = @DNumber)
+        BEGIN
+            DELETE FROM Department WHERE DNumber = @DNumber
+            DELETE FROM Dept_Locations WHERE DNUmber = @DNumber
+            DELETE FROM Project WHERE DNum = @DNumber
+            DELETE FROM Works_on WHERE Pno IN (SELECT PNumber FROM Project WHERE DNum = @DNumber)
+
+            UPDATE Employee SET Dno = NULL WHERE Dno = @DNumber
+
+            SELECT 'Department deleted successfully! :)' AS 'Message'
+        END
+    ELSE
+        BEGIN
+            THROW 51000, 'Department number not found.', 1
+        END
+END
+GO
+
+-- Procedure E:
+CREATE PROCEDURE [dbo].usp_GetDepartment (@DNumber INT) AS
+BEGIN
+    SELECT Department.*, COUNT(*) AS EmpCount
+    FROM Department
+        INNER JOIN Employee ON Department.DNumber = Employee.Dno
+    WHERE Department.DNumber = @DNumber
+    GROUP BY Department.DNumber, Department.DName, Department.MgrSSN, Department.MgrStartDate
+END
+GO
+
+-- Procedure F:
+CREATE PROCEDURE [dbo].usp_GetAllDepartments
+AS
+BEGIN
+    SELECT d.*, COUNT(*) AS EmpCount
+    FROM Department d
+    LEFT JOIN Employee e ON d.DNumber = e.Dno
+    GROUP BY d.DNumber, d.DName, d.MgrSSN, d.MgrStartDate
+END
+GO
 
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,187 @@
+import java.sql.*;
+        import java.util.Scanner;
+
 public class Main {
+
+    private static Connection conn;
+    private static Scanner scanner;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        scanner = new Scanner(System.in);
+        String dbURL = "jdbc:sqlserver://CHIASEED-LAPTOP;databaseName=Company;encrypt=true;trustServerCertificate=true;";
+        try {
+            conn = DriverManager.getConnection(dbURL, "test", "test");
+            System.out.println("Connected to DB.");
+
+            boolean exit = false;
+            while (!exit) {
+                System.out.println("Select an option:");
+                System.out.println("1. Add a department");
+                System.out.println("2. Update a department name");
+                System.out.println("3. Update a department manager");
+                System.out.println("4. Delete a department");
+                System.out.println("5. Get department by number");
+                System.out.println("6. Get all departments");
+                System.out.println("7. Exit");
+
+                int option = scanner.nextInt();
+                scanner.nextLine();
+
+                switch (option) {
+                    case 1:
+                        createDepartment();
+                        break;
+                    case 2:
+                        updateDepartmentName();
+                        break;
+                    case 3:
+                        updateDepartmentManager();
+                        break;
+                    case 4:
+                        deleteDepartment();
+                        break;
+                    case 5:
+                        getDepartmentByNumber();
+                        break;
+                    case 6:
+                        getAllDepartments();
+                        break;
+                    case 7:
+                        exit = true;
+                        break;
+                    default:
+                        System.out.println("Select a valid option, please.");
+                }
+            }
+
+            conn.close();
+            System.out.println("Disconnected from DB.");
+        } catch (SQLException ex) {
+            System.out.println("Error: " + ex.getMessage());
+        }
+    }
+
+    public static void createDepartment() {
+        try {
+            System.out.print("Enter department name: ");
+            String dName = scanner.nextLine();
+            System.out.print("Enter manager SSN: ");
+            int mgrSSN = scanner.nextInt();
+
+            CallableStatement stmt = conn.prepareCall("{CALL usp_CreateDepartment(?, ?)}");
+            stmt.setString(1, dName);
+            stmt.setInt(2, mgrSSN);
+//            stmt.registerOutParameter(3, Types.INTEGER);
+
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                System.out.println("Department added! :) The department number is " + rs.getInt("DNumber"));
+            }
+
+        } catch (SQLException ex) {
+            System.out.println("Error: " + ex.getMessage());
+        }
+    }
+
+    public static void updateDepartmentName() {
+        try {
+            System.out.print("Enter department number: ");
+            int dNumber = scanner.nextInt();
+            scanner.nextLine();
+            System.out.print("Enter new department name: ");
+            String dName = scanner.nextLine();
+
+            CallableStatement stmt = conn.prepareCall("{CALL usp_UpdateDepartmentName(?, ?)}");
+            stmt.setInt(1, dNumber);
+            stmt.setString(2, dName);
+
+            stmt.execute();
+
+            System.out.println("Department name updated! :)");
+        } catch (SQLException ex) {
+            System.out.println("An error occurred: " + ex.getMessage());
+        }
+    }
+
+    public static void updateDepartmentManager() {
+        try {
+            System.out.print("Enter department number: ");
+            int dNumber = scanner.nextInt();
+            scanner.nextLine();
+            System.out.print("Enter new manager SSN: ");
+            int mgrSSN = scanner.nextInt();
+            CallableStatement stmt = conn.prepareCall("{call usp_UpdateDepartmentManager(?, ?)}");
+            stmt.setInt(1, dNumber);
+            stmt.setInt(2, mgrSSN);
+
+            stmt.execute();
+
+            System.out.println("Department manager updated! :)");
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static void deleteDepartment() {
+        try {
+            System.out.println("Enter department number:");
+            int dNumber = scanner.nextInt();
+
+            CallableStatement stmt = conn.prepareCall("{call usp_DeleteDepartment(?)}");
+            stmt.setInt(1, dNumber);
+
+            stmt.execute();
+
+            System.out.println("Department deleted successfully.");
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static void getDepartmentByNumber() {
+        try {
+            System.out.println("Enter department number:");
+            int dNumber = scanner.nextInt();
+
+            CallableStatement stmt = conn.prepareCall("{call usp_GetDepartment(?)}");
+            stmt.setInt(1, dNumber);
+
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                System.out.println("Department Number: " + rs.getInt("DNumber"));
+                System.out.println("Department Name: " + rs.getString("DName"));
+                System.out.println("Manager SSN: " + rs.getInt("MgrSSN"));
+                System.out.println("Manager Start Date: " + rs.getDate("MgrStartDate"));
+                System.out.println("Employee Count: " + rs.getInt("EmpCount"));
+            } else {
+                System.out.println("Department not found. :(");
+            }
+
+            rs.close();
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static void getAllDepartments() {
+        try {
+            CallableStatement stmt = conn.prepareCall("{call usp_GetAllDepartments()}");
+
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                System.out.println("Department Number: " + rs.getInt("DNumber"));
+                System.out.println("Department Name: " + rs.getString("DName"));
+                System.out.println("Manager SSN: " + rs.getInt("MgrSSN"));
+                System.out.println("Manager Start Date: " + rs.getDate("MgrStartDate"));
+                System.out.println("Employee Count: " + rs.getInt("EmpCount"));
+                System.out.println("-----------------------");
+            }
+
+            rs.close();
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
-
-// TODO: Write a small .net or Java application which uses the stored procedures to access the department table of the Company database.
